42faeb091dfa0694a35c2317853640f1793dfbaf
xovest <79008917+xovest@users.noreply.github.com>
2021-10-31 15:48:35 +0200

    just linking and adding stuff

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,20 @@
+import { ActionTypes } from "../constants/action-types";
+
+const initialState = {
+  products: [
+    {
+      id: 1,
+      title: 'Ryan',
+      category: 'programmer'
+    }
+  ]
+};
+
+export const productReducer = (state, { type, payload }) => {
+  switch (type) {
+    case ActionTypes.SET_PRODUCTS:
+      return state;
+    default:
+      return state;
+  }
+};
