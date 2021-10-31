ce513040c3fe8869b2b2ddd19ac2930865af5f2a
xovest <79008917+xovest@users.noreply.github.com>
2021-10-31 20:04:28 +0200

    works perfect, but damn that css

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 
@@ -8,18 +9,20 @@ export default function ProductComponent() {
     const { id, title, image, price, category } = prod;
     return (
       <div className="four column wide" key={id}>
-        <div className="ui link cards">
-          <div className="card">
-            <div className="image">
-              <img src={ image } alt={ title } />
-            </div>
-            <div className="content">
-              <div className="header">{title}</div>
-              <div className="meta price">$ { price }</div>
-              <div className="meta">{ category }</div>
+        <Link to={`/product/${id}`}>
+          <div className="ui link cards">
+            <div className="card">
+              <div className="image">
+                <img src={ image } alt={ title } />
+              </div>
+              <div className="content">
+                <div className="header">{title}</div>
+                <div className="meta price">$ { price }</div>
+                <div className="meta">{ category }</div>
+              </div>
             </div>
           </div>
-        </div>
+        </Link>
       </div>
     );
   });
--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeSelectedProduct, selectedProduct } from '../redux/actions/productActions';
 
 export default function ProductDetail() {
+  const product = useSelector(state => state.product);
+  const { image, title, price, category, description } = product;
+  const {productId} = useParams();
+  const dispatch = useDispatch();
+
+  const fetchProductDetail = async () => {
+    const res = await axios
+      .get(`https://fakestoreapi.com/products/${productId}`)
+      .catch(err => console.log('Err', err));
+
+      dispatch(selectedProduct(res.data));
+  };
+
+  useEffect(() => {
+    if (productId && productId !== '') fetchProductDetail();
+    return () => {
+      dispatch(removeSelectedProduct());
+    };
+  }, [productId]);
+
   return (
-    <div>
-      <h1>ProductDetail</h1>
+    <div className="ui grid container">
+      {Object.keys(product).length === 0 ? (
+        <div>...Loading</div>
+      ) : (
+        <div className="ui placeholder segment">
+          <div className="ui two column stackable center aligned grid">
+            <div className="ui vertical divider">AND</div>
+            <div className="middle aligned row">
+              <div className="column lp">
+                <img className="ui fluid image" src={image} />
+              </div>
+              <div className="column rp">
+                <h1>{title}</h1>
+                <h2>
+                  <a className="ui teal tag label">${price}</a>
+                </h2>
+                <h3 className="ui brown block header">{category}</h3>
+                <p>{description}</p>
+                <div className="ui vertical animated button" tabIndex="0">
+                  <div className="hidden content">
+                    <i className="shop icon"></i>
+                  </div>
+                  <div className="visible content">Add to Cart</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
